472eefc41564acf24c384aaeec03304fd7d1ebc4
Tushar Sharma <tushar123cse@gmail.com>
2020-04-28 12:55:47 +0530

    logic to display authorName

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
@@ -9,6 +10,9 @@ class CoursesPage extends React.Component {
   componentDidMount() {
     this.props.actions.loadCourses().catch((error) => {
       alert("Loading courses failed " + error);
+    });
+    this.props.actions.loadAuthors().catch((error) => {
+      alert("Loading authors failed " + error);
     });
   }
 
@@ -29,13 +33,26 @@ CoursesPage.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    coursesyy: state.coursesy,
+    coursesyy:
+      state.authors.length === 0
+        ? []
+        : state.coursesy.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch),
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
   };
 }
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import coursesy from "./courseReducer";
+import authors from "./authorReducer";
 
 const rootReducer = combineReducers({
   coursesy,
+  authors,
 });
 
 export default rootReducer;

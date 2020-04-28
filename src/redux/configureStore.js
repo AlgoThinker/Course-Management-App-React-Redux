eecc73d8d923bd4735dd6587667c877e261d35ad
Tushar Sharma <tushar123cse@gmail.com>
2020-04-28 10:50:05 +0530

    added coursesList presentation component and Redux-thunk to make APi calls

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootRedcer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
   const composeEnhancers =
@@ -9,6 +10,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootRedcer,
     initialState,
-    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }

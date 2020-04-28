b08c8492f280561175e68a214a93f18cf9dba2ad
Tushar Sharma <tushar123cse@gmail.com>
2020-04-28 21:00:54 +0530

    converted class comp to functional comp using hooks

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { coursesyy, authors, loadAuthors, loadCourses } = this.props;
+function ManageCoursePage({ coursesyy, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
     if (coursesyy.length === 0) {
       loadCourses().catch((error) => {
         alert("Loading courses failed " + error);
@@ -18,15 +17,13 @@ class ManageCoursePage extends React.Component {
         alert("Loading authors failed " + error);
       });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {

66ae4bf6f350b035f642a7cc72a47fb0403763ea
Tushar Sharma <tushar123cse@gmail.com>
2020-04-28 20:21:32 +0530

    optimized wasteful network calls and created centralized initial state

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -8,12 +8,18 @@ import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
   componentDidMount() {
-    this.props.actions.loadCourses().catch((error) => {
-      alert("Loading courses failed " + error);
-    });
-    this.props.actions.loadAuthors().catch((error) => {
-      alert("Loading authors failed " + error);
-    });
+    const { coursesyy, authors, actions } = this.props;
+    if (coursesyy.length === 0) {
+      actions.loadCourses().catch((error) => {
+        alert("Loading courses failed " + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        alert("Loading authors failed " + error);
+      });
+    }
   }
 
   render() {
@@ -27,6 +33,7 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
   coursesyy: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 };

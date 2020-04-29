ddf2770efc41a27bf6ec39635644e275faca35d1
Tushar Sharma <tushar123cse@gmail.com>
2020-04-29 09:27:13 +0530

    support added to show spinner during network delays

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
 
 class CoursesPage extends React.Component {
   state = {
@@ -31,14 +32,20 @@ class CoursesPage extends React.Component {
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
-        <button
-          style={{ marginBottom: 20 }}
-          className="btn btn-primary add-course"
-          onClick={() => this.setState({ redirectToAddCoursePage: true })}
-        >
-          Add Course
-        </button>
-        <CourseList courses={this.props.coursesyy} />
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList courses={this.props.coursesyy} />
+          </>
+        )}
       </>
     );
   }
@@ -48,6 +55,7 @@ CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   coursesyy: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -63,6 +71,7 @@ function mapStateToProps(state) {
             };
           }),
     authors: state.authors,
+    loading: state.apiCallsInProgress > 0,
   };
 }
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import coursesy from "./courseReducer";
 import authors from "./authorReducer";
-
+import apiCallsInProgress from "./apiStatusReducer";
 const rootReducer = combineReducers({
   coursesy,
   authors,
+  apiCallsInProgress,
 });
 
 export default rootReducer;
